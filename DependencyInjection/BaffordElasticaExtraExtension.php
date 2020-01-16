eaa39e1a712704611eacf3453f242f18b64271f3
John Bafford <john@bafford.com>
2020-01-16 16:55:59 -0500

    Remove unnecessary Configuration

--- DependencyInjection/BaffordElasticaExtraExtension.php
+++ DependencyInjection/BaffordElasticaExtraExtension.php
@@ -14,12 +14,7 @@ class BaffordElasticaExtraExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-        
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-        
-        $container->setParameter('bafford_elastica_extra.config', $config);
     }
 }
